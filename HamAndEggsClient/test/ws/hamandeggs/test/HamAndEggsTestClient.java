558e9e33d7287802e811c669ac4adcbd0de446c6
superdids <didrik.aubert@gmail.com>
2016-10-02 08:15:21 -0700

    Initial commit.

---
+++ HamAndEggsClient/test/ws/hamandeggs/test/HamAndEggsTestClient.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.hamandeggs.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author superdids
+ */
+public class HamAndEggsTestClient {
+
+    
+    @Test
+    public void testHamAndEggs() {
+        int result = hamandeggsOperation(3);
+        assertEquals(result, 3);
+    }
+    
+    private static int hamandeggsOperation(int n) {
+        org.netbeans.j2ee.wsdl.hamandeggsbpel.src.hamandeggs.HamandeggsService service = new org.netbeans.j2ee.wsdl.hamandeggsbpel.src.hamandeggs.HamandeggsService();
+        org.netbeans.j2ee.wsdl.hamandeggsbpel.src.hamandeggs.HamandeggsPortType port = service.getHamandeggsPort();
+        return port.hamandeggsOperation(n);
+    }
+}
